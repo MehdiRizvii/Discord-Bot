e4a7c7af1f5fc04c660db746945025931de37b22
mehdi <mehdirizvi03@gmail.com>
2020-10-07 14:30:11 -0400

    Bot shell made

---
+++ main.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready',()=> {
+    console.log('Rem is Online')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NzYzNDYzMTM5ODQ0OTQ3OTg4.X34EdA.lfrYogI7zcX2C572SLWRhb3qBqY');
